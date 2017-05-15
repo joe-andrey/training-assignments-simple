7093b1e74adb6dab85108dc7168988a96e040aa6
Joe D. Andrey <joe.andrey@btpn.com>
2017-05-15 14:48:16 +0700

    fix Saving Account Transfer

--- src/java/eu/sig/training/ch04/SavingsAccount.java
+++ src/java/eu/sig/training/ch04/SavingsAccount.java
@@ -9,27 +9,36 @@ public class SavingsAccount {
     public Transfer makeTransfer(String counterAccount, Money amount) 
         throws BusinessException {
         // 1. Assuming result is 9-digit bank account number, validate 11-test:
-        int sum = 0; // <1>
+        int sum = calculateCheckSum(counterAccount);
+        if (sum % 11 == 0) {
+            // 2. Look up counter account and make transfer object:
+            return doTransfer(counterAccount, amount);
+        } else {
+            throw new BusinessException("Invalid account number!!");
+        }
+    }
+
+	private Transfer doTransfer(String counterAccount, Money amount) throws BusinessException {
+		CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
+		Transfer result = new Transfer(this, acct, amount); // <2>
+		// 3. Check whether withdrawal is to registered counter account:
+		if (result.getCounterAccount().equals(this.registeredCounterAccount)) 
+		{
+		    return result;
+		} else {
+		    throw new BusinessException("Counter-account not registered!");
+		}
+	}
+
+	private int calculateCheckSum(String counterAccount) {
+		int sum = 0; // <1>
         for (int i = 0; i < counterAccount.length(); i++) {
             char character = counterAccount.charAt(i);
             int characterValue = Character.getNumericValue(character);
             sum = sum + (9 - i) * characterValue;
         }
-        if (sum % 11 == 0) {
-            // 2. Look up counter account and make transfer object:
-            CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
-            Transfer result = new Transfer(this, acct, amount); // <2>
-            // 3. Check whether withdrawal is to registered counter account:
-            if (result.getCounterAccount().equals(this.registeredCounterAccount)) 
-            {
-                return result;
-            } else {
-                throw new BusinessException("Counter-account not registered!");
-            }
-        } else {
-            throw new BusinessException("Invalid account number!!");
-        }
-    }
+		return sum;
+	}
 
     public void addInterest() {
         Money interest = balance.multiply(INTEREST_PERCENTAGE);
