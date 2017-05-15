abfa1a7bce7c062e3f872a9f25fb1a0c24f5108f
angga.raditya@btpn.com <angga.raditya@btpn.com>
2017-05-15 15:06:14 +0700

    'FlagFactory'

--- src/java/eu/sig/training/ch04/CheckingAccount.java
+++ src/java/eu/sig/training/ch04/CheckingAccount.java
@@ -9,25 +9,43 @@ public class CheckingAccount {
     public Transfer makeTransfer(String counterAccount, Money amount)
         throws BusinessException {
         // 1. Check withdrawal limit:
-        if (amount.greaterThan(this.transferLimit)) {
-            throw new BusinessException("Limit exceeded!");
-        }
+        validateLimit(amount);
         // 2. Assuming result is 9-digit bank account number, validate 11-test:
         int sum = 0;
-        for (int i = 0; i < counterAccount.length(); i++) {
-            char character = counterAccount.charAt(i);
-            int characterValue = Character.getNumericValue(character);
-            sum = sum + (9 - i) * characterValue;
+        return validateDigits(counterAccount, amount, sum);
+    }
+
+	private Transfer validateDigits(String counterAccount, Money amount, int sum)
+			throws BusinessException {
+		for (int i = 0; i < counterAccount.length(); i++) {
+            sum = tremendousMultiply(counterAccount, sum, i);
         }
         if (sum % 11 == 0) {
             // 3. Look up counter account and make transfer object:
-            CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
-            Transfer result = new Transfer(this, acct, amount);
-            return result;
+            return createValidatedTransfer(counterAccount, amount);
         } else {
             throw new BusinessException("Invalid account number!");
         }
-    }
+	}
+
+	private Transfer createValidatedTransfer(String counterAccount, Money amount) {
+		CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
+		Transfer result = new Transfer(this, acct, amount);
+		return result;
+	}
+
+	private void validateLimit(Money amount) throws BusinessException {
+		if (amount.greaterThan(this.transferLimit)) {
+            throw new BusinessException("Limit exceeded!");
+        }
+	}
+
+	private int tremendousMultiply(String counterAccount, int sum, int i) {
+		char character = counterAccount.charAt(i);
+		int characterValue = Character.getNumericValue(character);
+		sum = sum + (9 - i) * characterValue;
+		return sum;
+	}
 
     public void addInterest() {
         Money interest = balance.multiply(INTEREST_PERCENTAGE);
