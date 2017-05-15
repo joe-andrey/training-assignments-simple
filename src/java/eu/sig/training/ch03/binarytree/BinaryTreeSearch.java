bf0e5dd3548a3ae6d5cd92ae31427ee6f6f81f79
Joe D. Andrey <joe.andrey@btpn.com>
2017-05-15 14:41:57 +0700

    foptimize BST

--- src/java/eu/sig/training/ch03/binarytree/BinaryTreeSearch.java
+++ src/java/eu/sig/training/ch03/binarytree/BinaryTreeSearch.java
@@ -10,21 +10,21 @@ public class BinaryTreeSearch {
         } else {
             if (nodeValue < node.getValue()) {
                 BinaryTreeNode<Integer> left = node.getLeft();
-                if (left == null) {
-                    throw new TreeException("Value not found in tree!");
-                } else {
-                    return 1 + calculateDepth(left, nodeValue);
-                }
+                return explore(nodeValue, left);
             } else {
                 BinaryTreeNode<Integer> right = node.getRight();
-                if (right == null) {
-                    throw new TreeException("Value not found in tree!");
-                } else {
-                    return 1 + calculateDepth(right, nodeValue);
-                }
+                return explore(nodeValue, right);
             }
         }
     }
     // end::calculateDepth[]
+
+	private static int explore(int nodeValue, BinaryTreeNode<Integer> left) {
+		if (left == null) {
+		    throw new TreeException("Value not found in tree!");
+		} else {
+		    return 1 + calculateDepth(left, nodeValue);
+		}
+	}
 
 }
